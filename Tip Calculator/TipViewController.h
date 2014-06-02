43efbfae951e54175a5df2a96f408f5a926ddea6
Tyler Miller <tylermiller@Tylers-MacBook-Pro.local>
2014-06-01 20:46:46 -0700

    First commit

---
+++ Tip Calculator/TipViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipViewController.h
+//  Tip Calculator
+//
+//  Created by Tyler Miller on 5/28/14.
+//  Copyright (c) 2014 Tyler Miller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipViewController : UIViewController
+
+@end
